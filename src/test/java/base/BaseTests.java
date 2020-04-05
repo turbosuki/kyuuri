768315662633f37b0c173af187118547a6a23185
turbosuki <57754617+turbosuki@users.noreply.github.com>
2020-04-05 19:32:48 +0100

    Added WebDriverEventListener class for Chapter 13, added some logging

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -12,17 +13,16 @@ import org.testng.annotations.BeforeClass;
 
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTests
 {
-	private WebDriver driver;
+	private EventFiringWebDriver driver;
 	private String baseUrl = "https://the-internet.herokuapp.com/";
 	protected HomePage homePage;
 
@@ -30,7 +30,8 @@ public class BaseTests
 	public void setUp()
 	{
 		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-		driver = new ChromeDriver();
+		driver = new EventFiringWebDriver(new ChromeDriver());
+		driver.register(new EventReporter());
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		goHome();
